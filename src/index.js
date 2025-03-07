84e4b41f4b2686c4162ed7901188f7762ddb8ba7
Qomarul Hasan <qomarhasan@protonmail.com>
2025-03-07 23:42:56 +0600

    deleted: files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './res/css/base.module.css';
-
-const root = ReactDOM.createRoot(document.getElementById('appContainer'));
-root.render(
-    <App />
-);
